2bccd87eed5b59d1b0d853635b134327174e2e06
Areeba <khan6@stolaf.edu>
2021-09-19 11:26:55 -0500

    Adds more elif conditions in the if statements

--- control_structures.sh
+++ control_structures.sh
@@ -2,9 +2,15 @@
 if [ $1 -gt 100 ]
 then
     echo You entered a number larger than 100
-fi
-
-if (($1 == 0))
+elif (($1 == 0))
 then
     echo Your number is 0
+elif (($1 < 0))
+then
+    echo Your number is negative
+elif(($1 <= 100))
+then
+    echo Your number is less than or equal to 100
+else
+    echo Your number is not a real number
 fi
