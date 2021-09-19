bd1d7d21bdc297d8d8f6fe93e62cd08fd2314c5d
Areeba <khan6@stolaf.edu>
2021-09-19 11:45:38 -0500

    Uses printf instead of echo

--- until.sh
+++ until.sh
@@ -2,13 +2,15 @@
 
 for var in {0..5}
 do
-    echo $var
     if (($var % 2 == 0))
     then
-	echo $var is even number
+	printf "%d is even number and" $var
     else
 	echo $var is odd number
+	continue
     fi
-    
+
+    ret=$(($var/2))
+    echo divides by 2 by $ret
 done
 echo Done
