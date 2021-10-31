2f6a8ef47a3642a908c19541332f1632fb693e9a
Areeba <khan6@stolaf.edu>
2021-10-31 17:55:04 -0500

    no significant changes

--- foobar.sh
+++ foobar.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-var1=foo
+export var1=foo
 echo $var1 created
+
 var2=bar
 echo $var2 created
 
-export var1
+export var1=100
 echo variales created
 
 
-var=$(ls)
-echo $var
+#var=$(ls)
+#echo $var
